f8fa263b867da136f44b5432ecc81744109048c9
Daniel Sucno <daniksucno363@gmail.com>
2026-01-05 15:53:00 -0300

    feat: add webchat loader

---
+++ main.js
@@ -0,0 +1,22 @@
+(async function () {
+    try {
+        const res = await fetch(
+            'https://cdn.jsdelivr.net/gh/danik363/web-chat-loader@main/main.js?t=' + Date.now()
+        );
+
+        const { channel } = await res.json();
+        if (!channel) return;
+
+        if (document.getElementById('livechat')) return;
+
+        const s = document.createElement('script');
+        s.defer = true;
+        s.src = 'https://livechat.hibot.us/hibot-chat.min.js';
+        s.dataset.channel = channel;
+        s.id = 'livechat';
+
+        document.head.appendChild(s);
+    } catch (e) {
+        console.error('Hibot loader error', e);
+    }
+})();
